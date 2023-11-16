14c945ea801cd601a8cdb3b37a60c9eeeeef3213
Hans Ramos <97885787+Ramos-H@users.noreply.github.com>
2023-11-16 23:14:14 +0800

    Moved login button onclick() logic into dedicated function

--- app/src/main/java/com/itg3/grp1/mobdevproject/MainActivity.kt
+++ app/src/main/java/com/itg3/grp1/mobdevproject/MainActivity.kt
@@ -9,61 +9,77 @@ import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    lateinit var fieldEmail : EditText
+    lateinit var fieldPass : EditText
+
+    lateinit var btnLogin : Button
+    lateinit var btnRegister : Button
+
+    lateinit var validationTextEmail : TextView
+    lateinit var validationTextPass : TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login_page)
-        var emailEditText = findViewById<EditText>(R.id.etEmail)
-        var passEditText = findViewById<EditText>(R.id.etPassword)
-        var loginBtn = findViewById<Button>(R.id.loginBtn)
-        var regBtn = findViewById<Button>(R.id.regBtn)
-        val validationTextEmail = findViewById<TextView>(R.id.validationTextView)
-        val validationTextPass = findViewById<TextView>(R.id.validationTextView2)
-        val text = "Navigated to Register"
-        val validTxt = "LOGIN SUCCESSFUL"
+
+        fieldEmail = findViewById<EditText>(R.id.etEmail)
+        fieldPass = findViewById<EditText>(R.id.etPassword)
+        btnLogin = findViewById<Button>(R.id.loginBtn)
+        btnRegister = findViewById<Button>(R.id.regBtn)
+        validationTextEmail = findViewById<TextView>(R.id.validationTextView)
+        validationTextPass = findViewById<TextView>(R.id.validationTextView2)
+
         validationTextEmail.visibility = View.INVISIBLE
         validationTextPass.visibility = View.INVISIBLE
 
-        fun isValidEmail(email: String): Boolean {
-            return email.length <= 255 && android.util.Patterns.EMAIL_ADDRESS.matcher(email)
-                .matches()
+        btnRegister.setOnClickListener() {
+            Toast.makeText(this, "Navigated to Register", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private fun isValidEmail(email: String): Boolean
+    {
+        return email.length <= 255 && android.util.Patterns.EMAIL_ADDRESS.matcher(email)
+            .matches()
+    }
+
+    private fun isValidPassword(password: String): Boolean
+    {
+        // Condition 1: Password must be at least 8 characters
+        return password.length >= 8 && password.matches(Regex("^[a-zA-Z0-9]+$"))
+    }
+
+    private fun setFieldValidationState(field: EditText, validationDisplay: TextView,
+                                        visibility: Int, message: String? = null)
+    {
+        field.error = message
+        validationDisplay.visibility = visibility
+        validationDisplay.text = message
+    }
+
+    fun login(view: View)
+    {
+        val email = fieldEmail.text.toString()
+        val pass = fieldPass.text.toString()
+
+        if (email.isEmpty()) {
+            setFieldValidationState(fieldEmail, validationTextEmail, View.VISIBLE,"Email is required")
+        } else if (!isValidEmail(email)) {
+            setFieldValidationState(fieldEmail, validationTextEmail, View.VISIBLE,"Invalid email format")
+        } else {
+            setFieldValidationState(fieldEmail, validationTextEmail, View.INVISIBLE)
         }
 
-        fun setFieldValidationState(field: EditText, validationDisplay: TextView, visibility: Int, message: String? = null) {
-            field.error = message
-            validationDisplay.visibility = visibility
-            validationDisplay.text = message
+        if (pass.isEmpty()) {
+            setFieldValidationState(fieldPass, validationTextPass, View.VISIBLE,"Password is required")
+        } else if (!isValidPassword(pass)) {
+            setFieldValidationState(fieldPass, validationTextPass, View.VISIBLE,"Password must be at least 8 characters")
+        } else {
+            setFieldValidationState(fieldPass, validationTextPass, View.INVISIBLE)
         }
 
-        fun isValidPassword(password: String): Boolean {
-            // Condition 1: Password must be at least 8 characters
-            return password.length >= 8 && password.matches(Regex("^[a-zA-Z0-9]+$"))
-        }
-        loginBtn.setOnClickListener() {
-            val email = emailEditText.text.toString()
-            val pass = passEditText.text.toString()
-
-            if (email.isEmpty()) {
-                setFieldValidationState(emailEditText, validationTextEmail, View.VISIBLE,"Email is required")
-            } else if (!isValidEmail(email)) {
-                setFieldValidationState(emailEditText, validationTextEmail, View.VISIBLE,"Invalid email format")
-            } else {
-                setFieldValidationState(emailEditText, validationTextEmail, View.INVISIBLE)
-            }
-
-            if (pass.isEmpty()) {
-                setFieldValidationState(passEditText, validationTextPass, View.VISIBLE,"Password is required")
-            } else if (!isValidPassword(pass)) {
-                setFieldValidationState(passEditText, validationTextPass, View.VISIBLE,"Password must be at least 8 characters")
-            } else {
-                setFieldValidationState(passEditText, validationTextPass, View.INVISIBLE)
-            }
-
-            if (isValidEmail(email) && isValidPassword(pass)) {
-                Toast.makeText(this, "LOGIN SUCCESSFUL", Toast.LENGTH_LONG).show()
-            }
-        }
-        regBtn.setOnClickListener() {
-            Toast.makeText(this, text, Toast.LENGTH_LONG).show()
+        if (isValidEmail(email) && isValidPassword(pass)) {
+            Toast.makeText(this, "LOGIN SUCCESSFUL", Toast.LENGTH_LONG).show()
         }
     }
 }
