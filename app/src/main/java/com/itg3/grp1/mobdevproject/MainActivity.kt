33e738df4e7e351e2e860e7d55e588d34a9d7534
Hans Ramos <97885787+Ramos-H@users.noreply.github.com>
2023-11-16 23:04:45 +0800

    Repurposed resetFieldState() into more general setFieldValidationState()

--- app/src/main/java/com/itg3/grp1/mobdevproject/MainActivity.kt
+++ app/src/main/java/com/itg3/grp1/mobdevproject/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.itg3.grp1.mobdevproject
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -29,9 +28,10 @@ class MainActivity : AppCompatActivity() {
                 .matches()
         }
 
-        fun resetFieldState(editText: EditText, validationText: TextView) {
-            editText.setError(null)
-            validationText.visibility = View.INVISIBLE
+        fun setFieldValidationState(field: EditText, validationDisplay: TextView, visibility: Int, message: String? = null) {
+            field.error = message
+            validationDisplay.visibility = visibility
+            validationDisplay.text = message
         }
 
         fun isValidPassword(password: String): Boolean {
@@ -41,28 +41,23 @@ class MainActivity : AppCompatActivity() {
         loginBtn.setOnClickListener() {
             val email = emailEditText.text.toString()
             val pass = passEditText.text.toString()
+
             if (email.isEmpty()) {
-                emailEditText.setError("Email is required")
-                validationTextEmail.visibility = View.VISIBLE
-                validationTextEmail.text = "Email is required"
+                setFieldValidationState(emailEditText, validationTextEmail, View.VISIBLE,"Email is required")
             } else if (!isValidEmail(email)) {
-                emailEditText.setError("Invalid email")
-                validationTextEmail.visibility = View.VISIBLE
-                validationTextEmail.text = "Invalid email format"
+                setFieldValidationState(emailEditText, validationTextEmail, View.VISIBLE,"Invalid email format")
             } else {
-                resetFieldState(emailEditText, validationTextEmail)
+                setFieldValidationState(emailEditText, validationTextEmail, View.INVISIBLE)
             }
+
             if (pass.isEmpty()) {
-                passEditText.setError("Password is required")
-                validationTextPass.visibility = View.VISIBLE
-                validationTextPass.text = "Password is required"
+                setFieldValidationState(passEditText, validationTextPass, View.VISIBLE,"Password is required")
             } else if (!isValidPassword(pass)) {
-                validationTextPass.visibility = View.VISIBLE
-                validationTextPass.text = "Invalid pass format"
-                validationTextPass.text = "Password must be at least 8 characters."
+                setFieldValidationState(passEditText, validationTextPass, View.VISIBLE,"Password must be at least 8 characters")
             } else {
-                resetFieldState(passEditText, validationTextPass)
+                setFieldValidationState(passEditText, validationTextPass, View.INVISIBLE)
             }
+
             if (isValidEmail(email) && isValidPassword(pass)) {
                 Toast.makeText(this, "LOGIN SUCCESSFUL", Toast.LENGTH_LONG).show()
             }
