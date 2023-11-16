ddcfab1654f1ae1f7cbab578fa22e61959d5b5ea
Hans Ramos <97885787+Ramos-H@users.noreply.github.com>
2023-11-16 22:49:07 +0800

    Ran code autoformat

--- app/src/main/java/com/itg3/grp1/mobdevproject/MainActivity.kt
+++ app/src/main/java/com/itg3/grp1/mobdevproject/MainActivity.kt
@@ -15,7 +15,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.login_page)
         var emailEditText = findViewById<EditText>(R.id.etEmail)
         var passEditText = findViewById<EditText>(R.id.etPassword)
-        var loginBtn= findViewById<Button>(R.id.loginBtn)
+        var loginBtn = findViewById<Button>(R.id.loginBtn)
         var regBtn = findViewById<Button>(R.id.regBtn)
         val validationTextEmail = findViewById<TextView>(R.id.validationTextView)
         val validationTextPass = findViewById<TextView>(R.id.validationTextView2)
@@ -24,17 +24,19 @@ class MainActivity : AppCompatActivity() {
         validationTextEmail.visibility = View.INVISIBLE
         validationTextPass.visibility = View.INVISIBLE
 
-         fun isValidEmail(email: String): Boolean {
-            return email.length <= 255 && android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()
-
+        fun isValidEmail(email: String): Boolean {
+            return email.length <= 255 && android.util.Patterns.EMAIL_ADDRESS.matcher(email)
+                .matches()
         }
-         fun resetFieldState(editText: EditText, validationText: TextView) {
+
+        fun resetFieldState(editText: EditText, validationText: TextView) {
             editText.setError(null)
             validationText.visibility = View.INVISIBLE
         }
-         fun isValidPassword(password: String): Boolean {
+
+        fun isValidPassword(password: String): Boolean {
             // Condition 1: Password must be at least 8 characters
-             return password.length >= 8 && password.matches(Regex("^[a-zA-Z0-9]+$"))
+            return password.length >= 8 && password.matches(Regex("^[a-zA-Z0-9]+$"))
         }
         loginBtn.setOnClickListener() {
             val email = emailEditText.text.toString()
@@ -49,32 +51,28 @@ class MainActivity : AppCompatActivity() {
                 validationTextEmail.visibility = View.VISIBLE
                 validationTextEmail.text = "Invalid email format"
                 validationTextEmail.setTextColor(resources.getColor(android.R.color.holo_red_light))
-            }
-            else {
+            } else {
                 resetFieldState(emailEditText, validationTextEmail)
             }
-            if(pass.isEmpty()) {
+            if (pass.isEmpty()) {
                 passEditText.setError("Password is required")
                 validationTextPass.visibility = View.VISIBLE
                 validationTextPass.text = "Password is required"
                 validationTextPass.setTextColor(resources.getColor(android.R.color.holo_red_light))
-            }
-             else if(!isValidPassword(pass)){
+            } else if (!isValidPassword(pass)) {
                 validationTextPass.visibility = View.VISIBLE
                 validationTextPass.text = "Invalid pass format"
                 validationTextPass.text = "Password must be at least 8 characters."
                 validationTextPass.setTextColor(resources.getColor(android.R.color.holo_red_light))
-            }
-             else{
+            } else {
                 resetFieldState(passEditText, validationTextPass)
             }
             if (isValidEmail(email) && isValidPassword(pass)) {
                 Toast.makeText(this, "LOGIN SUCCESSFUL", Toast.LENGTH_LONG).show()
             }
         }
-        regBtn.setOnClickListener(){
+        regBtn.setOnClickListener() {
             Toast.makeText(this, text, Toast.LENGTH_LONG).show()
         }
-
     }
 }
