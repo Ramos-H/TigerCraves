894d500579d5dd0e6b0e81a995046c9ba916291e
Hans Ramos <97885787+Ramos-H@users.noreply.github.com>
2023-11-17 17:19:28 +0800

    Added constraints to database keys

--- app/src/main/java/com/itg3/grp1/mobdevproject/DatabaseHandler.kt
+++ app/src/main/java/com/itg3/grp1/mobdevproject/DatabaseHandler.kt
@@ -16,34 +16,37 @@ class DatabaseHandler(context: Context) : SQLiteOpenHelper(context, DATABASE_NAM
     override fun onCreate(db: SQLiteDatabase?)
     {
         val SQL_TBL_CREATE_USER = "CREATE TABLE ${TigerCraves.User.TABLE_NAME} (" +
-                "${TigerCraves.User.COL_ID} INTEGER PRIMARY KEY, " +
-                "${TigerCraves.User.COL_NAME_FIRST} TEXT, " +
+                "${TigerCraves.User.COL_ID} INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, " +
+                "${TigerCraves.User.COL_NAME_FIRST} TEXT NOT NULL, " +
                 "${TigerCraves.User.COL_NAME_MIDDLE} TEXT, " +
-                "${TigerCraves.User.COL_NAME_LAST} TEXT, " +
-                "${TigerCraves.User.COL_EMAIL} TEXT, " +
-                "${TigerCraves.User.COL_PASSWORD_HASH} TEXT, " +
-                "${TigerCraves.User.COL_DATE_REGISTERED} INTEGER" +
+                "${TigerCraves.User.COL_NAME_LAST} TEXT NOT NULL, " +
+                "${TigerCraves.User.COL_EMAIL} TEXT NOT NULL, " +
+                "${TigerCraves.User.COL_PASSWORD_HASH} TEXT NOT NULL, " +
+                "${TigerCraves.User.COL_DATE_REGISTERED} INTEGER DEFAULT(unixepoch()) NOT NULL" +
                 ")"
 
         val SQL_TBL_CREATE_LISTING = "CREATE TABLE ${TigerCraves.Listing.TABLE_NAME} (" +
-                "${TigerCraves.Listing.COL_ID} INTEGER PRIMARY KEY, " +
-                "${TigerCraves.Listing.COL_POSTER} INTEGER, " +
-                "${TigerCraves.Listing.COL_NAME} TEXT, " +
-                "${TigerCraves.Listing.COL_ADDRESS} TEXT, " +
+                "${TigerCraves.Listing.COL_ID} INTEGER PRIMARY KEY NOT NULL, " +
+                "${TigerCraves.Listing.COL_POSTER} INTEGER NOT NULL, " +
+                "${TigerCraves.Listing.COL_NAME} TEXT NOT NULL, " +
+                "${TigerCraves.Listing.COL_ADDRESS} TEXT NOT NULL, " +
                 "${TigerCraves.Listing.COL_PRICE_MIN} REAL, " +
                 "${TigerCraves.Listing.COL_PRICE_MAX} REAL, " +
                 "${TigerCraves.Listing.COL_RATING} REAL, " +
-                "${TigerCraves.Listing.COL_DATE_POSTED} INTEGER" +
+                "${TigerCraves.Listing.COL_DATE_POSTED} INTEGER DEFAULT(unixepoch()) NOT NULL, " +
+                "FOREIGN KEY (${TigerCraves.Listing.COL_POSTER}) REFERENCES ${TigerCraves.User.TABLE_NAME}(${TigerCraves.User.COL_ID}) ON DELETE CASCADE" +
                 ")"
 
         val SQL_TBL_CREATE_REVIEW = "CREATE TABLE ${TigerCraves.Review.TABLE_NAME} (" +
                 "${TigerCraves.Review.COL_ID} INTEGER PRIMARY KEY, " +
-                "${TigerCraves.Review.COL_POSTER} INTEGER, " +
-                "${TigerCraves.Review.COL_LISTING} INTEGER, " +
+                "${TigerCraves.Review.COL_POSTER} INTEGER NOT NULL, " +
+                "${TigerCraves.Review.COL_LISTING} INTEGER NOT NULL, " +
                 "${TigerCraves.Review.COL_RATING} REAL, " +
                 "${TigerCraves.Review.COL_TITLE} TEXT, " +
                 "${TigerCraves.Review.COL_CONTENT} TEXT, " +
-                "${TigerCraves.Review.COL_DATE_POSTED} INTEGER " +
+                "${TigerCraves.Review.COL_DATE_POSTED} INTEGER DEFAULT(unixepoch()) NOT NULL, " +
+                "FOREIGN KEY (${TigerCraves.Review.COL_POSTER}) REFERENCES ${TigerCraves.User.TABLE_NAME}(${TigerCraves.User.COL_ID}) ON DELETE CASCADE, " +
+                "FOREIGN KEY (${TigerCraves.Review.COL_LISTING}) REFERENCES ${TigerCraves.Listing.TABLE_NAME}(${TigerCraves.Listing.COL_ID}) ON DELETE CASCADE" +
                 ")"
 
         Log.d("SQL_Stuff", SQL_TBL_CREATE_USER)
