34acf1575649746e1fd32a4e069312b721bb0380
Hans Ramos <97885787+Ramos-H@users.noreply.github.com>
2023-11-17 18:54:16 +0800

    Moved User table creation and deletion code to User DbTable class

--- app/src/main/java/com/itg3/grp1/mobdevproject/DatabaseHandler.kt
+++ app/src/main/java/com/itg3/grp1/mobdevproject/DatabaseHandler.kt
@@ -4,6 +4,8 @@ import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 import android.util.Log
+import com.itg3.grp1.mobdevproject.DbTables.IDbTable
+import com.itg3.grp1.mobdevproject.DbTables.Users
 
 class DatabaseHandler(context: Context) : SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION)
 {
@@ -13,17 +15,12 @@ class DatabaseHandler(context: Context) : SQLiteOpenHelper(context, DATABASE_NAM
         val DATABASE_VERSION = 1
     }
 
+    lateinit var users: IDbTable
+
     override fun onCreate(db: SQLiteDatabase?)
     {
-        val SQL_TBL_CREATE_USER = "CREATE TABLE ${TigerCraves.User.TABLE_NAME} (" +
-                "${TigerCraves.User.COL_ID} INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, " +
-                "${TigerCraves.User.COL_NAME_FIRST} TEXT NOT NULL, " +
-                "${TigerCraves.User.COL_NAME_MIDDLE} TEXT, " +
-                "${TigerCraves.User.COL_NAME_LAST} TEXT NOT NULL, " +
-                "${TigerCraves.User.COL_EMAIL} TEXT NOT NULL, " +
-                "${TigerCraves.User.COL_PASSWORD_HASH} TEXT NOT NULL, " +
-                "${TigerCraves.User.COL_DATE_REGISTERED} INTEGER DEFAULT(unixepoch()) NOT NULL" +
-                ")"
+        users = Users()
+        users.createTable(db)
 
         val SQL_TBL_CREATE_LISTING = "CREATE TABLE ${TigerCraves.Listing.TABLE_NAME} (" +
                 "${TigerCraves.Listing.COL_ID} INTEGER PRIMARY KEY NOT NULL, " +
@@ -34,7 +31,7 @@ class DatabaseHandler(context: Context) : SQLiteOpenHelper(context, DATABASE_NAM
                 "${TigerCraves.Listing.COL_PRICE_MAX} REAL, " +
                 "${TigerCraves.Listing.COL_RATING} REAL, " +
                 "${TigerCraves.Listing.COL_DATE_POSTED} INTEGER DEFAULT(unixepoch()) NOT NULL, " +
-                "FOREIGN KEY (${TigerCraves.Listing.COL_POSTER}) REFERENCES ${TigerCraves.User.TABLE_NAME}(${TigerCraves.User.COL_ID}) ON DELETE CASCADE" +
+                "FOREIGN KEY (${TigerCraves.Listing.COL_POSTER}) REFERENCES ${Users.TBL_NAME}(${Users.COL_ID}) ON DELETE CASCADE" +
                 ")"
 
         val SQL_TBL_CREATE_REVIEW = "CREATE TABLE ${TigerCraves.Review.TABLE_NAME} (" +
@@ -45,26 +42,22 @@ class DatabaseHandler(context: Context) : SQLiteOpenHelper(context, DATABASE_NAM
                 "${TigerCraves.Review.COL_TITLE} TEXT, " +
                 "${TigerCraves.Review.COL_CONTENT} TEXT, " +
                 "${TigerCraves.Review.COL_DATE_POSTED} INTEGER DEFAULT(unixepoch()) NOT NULL, " +
-                "FOREIGN KEY (${TigerCraves.Review.COL_POSTER}) REFERENCES ${TigerCraves.User.TABLE_NAME}(${TigerCraves.User.COL_ID}) ON DELETE CASCADE, " +
+                "FOREIGN KEY (${TigerCraves.Review.COL_POSTER}) REFERENCES ${Users.TBL_NAME}(${Users.COL_ID}) ON DELETE CASCADE, " +
                 "FOREIGN KEY (${TigerCraves.Review.COL_LISTING}) REFERENCES ${TigerCraves.Listing.TABLE_NAME}(${TigerCraves.Listing.COL_ID}) ON DELETE CASCADE" +
                 ")"
 
-        Log.d("SQL_Stuff", SQL_TBL_CREATE_USER)
         Log.d("SQL_Stuff", SQL_TBL_CREATE_LISTING)
         Log.d("SQL_Stuff", SQL_TBL_CREATE_REVIEW)
 
-        db?.execSQL(SQL_TBL_CREATE_USER)
         db?.execSQL(SQL_TBL_CREATE_LISTING)
         db?.execSQL(SQL_TBL_CREATE_REVIEW)
     }
 
     override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int)
     {
-        val SQL_TBL_DROP_USER = "DROP TABLE IF EXISTS ${TigerCraves.User.TABLE_NAME}"
         val SQL_TBL_DROP_LISTING = "DROP TABLE IF EXISTS ${TigerCraves.Listing.TABLE_NAME}"
         val SQL_TBL_DROP_REVIEW = "DROP TABLE IF EXISTS ${TigerCraves.Review.TABLE_NAME}"
 
-        db?.execSQL(SQL_TBL_DROP_USER)
         db?.execSQL(SQL_TBL_DROP_LISTING)
         db?.execSQL(SQL_TBL_DROP_REVIEW)
 
--- app/src/main/java/com/itg3/grp1/mobdevproject/TigerCraves.kt
+++ app/src/main/java/com/itg3/grp1/mobdevproject/TigerCraves.kt
@@ -4,18 +4,6 @@ import android.provider.BaseColumns
 
 object TigerCraves
 {
-    object User
-    {
-        val TABLE_NAME = "Users"
-        val COL_ID = "Id"
-        val COL_NAME_FIRST = "NameFirst"
-        val COL_NAME_MIDDLE = "NameMiddle"
-        val COL_NAME_LAST = "NameLast"
-        val COL_EMAIL = "Email"
-        val COL_PASSWORD_HASH = "PasswordHash"
-        val COL_DATE_REGISTERED = "DateRegistered"
-    }
-
     object Listing
     {
         val TABLE_NAME = "Listings"
