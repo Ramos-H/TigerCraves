95bfcd272e8615e394dff9b97add1241d5cf1b9a
clairecruuz <alessandracruzclaire@gmail.com>
2023-11-17 19:36:24 +0800

    Added ScrollView and an ImageView for the background of the restaurant details

---
+++ app/src/main/java/com/itg3/grp1/mobdevproject/DetailedListingActivity.kt
@@ -0,0 +1,15 @@
+package com.itg3.grp1.mobdevproject
+
+// DetailedListingActivity.kt
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class DetailedListingActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detailedlisting)
+
+        // Your additional logic or functionality can be added here
+    }
+}
