ce83a2bd105d1da2265962de40dd32fbdec147af
clairecruuz <alessandracruzclaire@gmail.com>
2023-11-17 22:45:12 +0800

    Added a function to show toast message "Navigated to Listings Page" when the back button is  clicked.

--- app/src/main/java/com/itg3/grp1/mobdevproject/DetailedListingActivity.kt
+++ app/src/main/java/com/itg3/grp1/mobdevproject/DetailedListingActivity.kt
@@ -1,7 +1,9 @@
 package com.itg3.grp1.mobdevproject
 
-// DetailedListingActivity.kt
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageButton
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class DetailedListingActivity : AppCompatActivity() {
@@ -10,6 +12,18 @@ class DetailedListingActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detailedlisting)
 
-        // Your additional logic or functionality can be added here
+        // Find views by their IDs
+        val backButton = findViewById<ImageButton>(R.id.btnBackToListings)
+
+        // Set click listener for the back button
+        backButton.setOnClickListener {
+            // Show a toast message when clicked
+            showToast("Navigated to Listings Page")
+        }
+    }
+
+    // Function to show a toast message
+    private fun showToast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
     }
 }
