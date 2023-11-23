56f6f35c04daee45039cf900ac72fca4546c8232
pngRMS <127420583+pngRMS@users.noreply.github.com>
2023-11-23 16:48:08 +0800

    - Modified UI layouts (_seperated the layouts between the bg_, modified activity_listing design) - Added a SearchView - Added Filter button - Added Sort button * Dialog Box content not fully developed *

--- app/src/main/java/com/itg3/grp1/mobdevproject/ListingActivity.kt
+++ app/src/main/java/com/itg3/grp1/mobdevproject/ListingActivity.kt
@@ -1,9 +1,18 @@
 package com.itg3.grp1.mobdevproject
 
+import android.app.Dialog
 import android.content.Intent
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.view.View
+import android.view.Window
+import android.widget.Button
+import android.widget.ImageButton
+import android.widget.RadioButton
+import android.widget.RadioGroup
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -12,12 +21,26 @@ import com.itg3.grp1.mobdevproject.data.models.Listing
 
 class ListingActivity: AppCompatActivity()
 {
+    private var selectedSortingOption: Int = R.id.rb1
+    private var selectedSortingOption2: Int = R.id.rb1
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_listing)
 
         val dbHelper = DatabaseHelper(this)
+        val showFilter = findViewById<Button>(R.id.filterbtn)
+        val showSort = findViewById<Button>(R.id.sortbtn)
+
+
+        showFilter.setOnClickListener(){
+        val message ="Sorting Direction"
+        showFiltering(message)
+        }
+        showSort.setOnClickListener(){
+            val message="Sorting Criteria"
+            showSorting(message)
+        }
 
         val user = dbHelper.users.getOne(intent.extras!!.getInt("userId"))
         val welcomeBanner: TextView = findViewById(R.id.welcomeBanner)
@@ -41,5 +64,70 @@ class ListingActivity: AppCompatActivity()
     {
         val intent = Intent(this, LoginActivity::class.java)
         startActivity(intent)
+    }
+
+    fun showFiltering(message:String){
+        val dialog = Dialog(this)
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        dialog.setCancelable(false)
+        dialog.setContentView(R.layout.cardview_filter)
+        dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+
+        val tvTitle=dialog.findViewById<TextView>(R.id.tvTitle)
+        val rb1=dialog.findViewById<RadioButton>(R.id.rb1)
+        val rb2=dialog.findViewById<RadioButton>(R.id.rb2)
+        val radioGroup = dialog.findViewById<RadioGroup>(R.id.radioGroup)
+        radioGroup.check(selectedSortingOption)
+
+        tvTitle.text = message
+
+        rb1.setOnClickListener {
+            Toast.makeText(this, "Filter: Ascending", Toast.LENGTH_SHORT).show()
+            selectedSortingOption = R.id.rb1 // Update the selected value
+            dialog.dismiss()
+        }
+
+        rb2.setOnClickListener {
+            Toast.makeText(this, "Filter: Descending", Toast.LENGTH_SHORT).show()
+            selectedSortingOption = R.id.rb2 // Update the selected value
+            dialog.dismiss()
+        }
+
+        dialog.show()
+    }
+    fun showSorting(message:String){
+        val dialog = Dialog(this)
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        dialog.setCancelable(false)
+        dialog.setContentView(R.layout.cardview_sort)
+        dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+
+        val tvTitle=dialog.findViewById<TextView>(R.id.tvTitle)
+        val rb1=dialog.findViewById<RadioButton>(R.id.rb1)
+        val rb2=dialog.findViewById<RadioButton>(R.id.rb2)
+        val rb3=dialog.findViewById<RadioButton>(R.id.rb3)
+        val radioGroup = dialog.findViewById<RadioGroup>(R.id.radioGroup)
+        radioGroup.check(selectedSortingOption2)
+
+        tvTitle.text = message
+
+        rb1.setOnClickListener {
+            Toast.makeText(this, "Name clicked", Toast.LENGTH_SHORT).show()
+            selectedSortingOption2 = R.id.rb1 // Update the selected value
+            dialog.dismiss()
+        }
+
+        rb2.setOnClickListener {
+            Toast.makeText(this, "Avg Rating clicked", Toast.LENGTH_SHORT).show()
+            selectedSortingOption2 = R.id.rb2 // Update the selected value
+            dialog.dismiss()
+        }
+        rb3.setOnClickListener {
+            Toast.makeText(this, "Price clicked", Toast.LENGTH_SHORT).show()
+            selectedSortingOption2 = R.id.rb3 // Update the selected value
+            dialog.dismiss()
+        }
+
+        dialog.show()
     }
 }
