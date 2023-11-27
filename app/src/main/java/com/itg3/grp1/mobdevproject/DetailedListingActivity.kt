f8077aad235fba5565e8cd9f64388d71868bb00d
caie <alessandracruzclaire@gmail.com>
2023-11-27 18:36:38 +0800

    Added functions to show the delete and edit dialogs

--- app/src/main/java/com/itg3/grp1/mobdevproject/DetailedListingActivity.kt
+++ app/src/main/java/com/itg3/grp1/mobdevproject/DetailedListingActivity.kt
@@ -12,8 +12,7 @@ import com.itg3.grp1.mobdevproject.data.DatabaseHelper
 import com.itg3.grp1.mobdevproject.data.models.Listing
 import com.itg3.grp1.mobdevproject.data.models.Review
 
-class DetailedListingActivity : AppCompatActivity()
-{
+class DetailedListingActivity : AppCompatActivity() {
     private var userId: Int? = -1
     private val dbHelper = DatabaseHelper(this)
     private var listing: Listing? = null
@@ -22,8 +21,7 @@ class DetailedListingActivity : AppCompatActivity()
 
     private lateinit var ratingValidationText: TextView
 
-    override fun onCreate(savedInstanceState: Bundle?)
-    {
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detailedlisting)
 
@@ -47,34 +45,29 @@ class DetailedListingActivity : AppCompatActivity()
         val reviews = dbHelper.reviews.getAll().filter { listingId == it.Listing.Id }
 
 
-        if(!reviews.isNullOrEmpty())
-        {
+        if (!reviews.isNullOrEmpty()) {
             val noReviewsText: TextView = findViewById(R.id.noReviewsText)
             noReviewsText.visibility = View.GONE
             initReviewAdapter(reviews)
         }
     }
 
-    private fun initReviewAdapter(reviews: List<Review>)
-    {
+    private fun initReviewAdapter(reviews: List<Review>) {
         val reviewRecycler = findViewById<RecyclerView>(R.id.reviewRecycler)
         reviewRecycler.visibility = View.VISIBLE
-        reviewRecycler.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+        reviewRecycler.layoutManager =
+            LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
 
-        if(reviewAdapter != null)
-        {
+        if (reviewAdapter != null) {
             reviewAdapter!!.dataset = reviews
             reviewAdapter!!.notifyDataSetChanged()
-        }
-        else
-        {
+        } else {
             reviewAdapter = ReviewAdapter(reviews)
             reviewRecycler.adapter = reviewAdapter
         }
     }
 
-    fun goToListingPage(view: View)
-    {
+    fun goToListingPage(view: View) {
         val intent = Intent(this, ListingActivity::class.java)
         intent.putExtra("userId", userId)
         startActivity(intent)
@@ -86,8 +79,7 @@ class DetailedListingActivity : AppCompatActivity()
     }
 
     // Function to show the Review Composer dialog
-    fun showReviewComposerDialog(view: View)
-    {
+    fun showReviewComposerDialog(view: View) {
         val dialogView = layoutInflater.inflate(R.layout.dialog_review_composer, null)
         val dialogBuilder = AlertDialog.Builder(this)
             .setView(dialogView)
@@ -116,19 +108,15 @@ class DetailedListingActivity : AppCompatActivity()
             val title = fieldTitle.text.toString().trim()
             val content = fieldContent.text.toString().trim()
 
-            if (validateInputFields(fieldTitle, fieldContent, rating))
-            {
+            if (validateInputFields(fieldTitle, fieldContent, rating)) {
                 // Both fields are valid, hide the dialog and show a toast
                 val poster = dbHelper.users.getOne(userId!!)
                 val newReview = Review(null, poster!!, listing!!, rating.toDouble(), title, content)
                 val newReviewId = dbHelper.reviews.add(newReview)
 
-                if(newReviewId.toInt() == -1)
-                {
+                if (newReviewId.toInt() == -1) {
                     showToast("Review cannot be posted. There must be a problem with the database.")
-                }
-                else
-                {
+                } else {
                     // Get fresh list of reviews
                     val reviews = dbHelper.reviews.getAll().filter { listing!!.Id == it.Listing.Id }
 
@@ -153,8 +141,11 @@ class DetailedListingActivity : AppCompatActivity()
     }
 
     // Function to validate input fields
-    private fun validateInputFields(fieldTitle: ValEditText, fieldContent: ValEditText, rating: Float): Boolean
-    {
+    private fun validateInputFields(
+        fieldTitle: ValEditText,
+        fieldContent: ValEditText,
+        rating: Float
+    ): Boolean {
         fieldTitle.error = null
         fieldContent.error = null
         clearValidationErrorsForRating()
@@ -162,27 +153,20 @@ class DetailedListingActivity : AppCompatActivity()
         var title = fieldTitle.text?.trim()
         var content = fieldContent.text?.trim()
 
-        if (title.isNullOrBlank())
-        {
+        if (title.isNullOrBlank()) {
             fieldTitle.error = "Title is required"
-        }
-        else if (title.length > 1024)
-        {
+        } else if (title.length > 1024) {
             fieldTitle.error = "Title cannot exceed 1024 characters"
         }
 
-        if (content.isNullOrBlank())
-        {
+        if (content.isNullOrBlank()) {
             fieldContent.error = "Content is required"
-        }
-        else if (content.length > 2048)
-        {
+        } else if (content.length > 2048) {
             fieldContent.error = "Content cannot exceed 2048 characters"
         }
 
         val noRatingGiven = rating <= 0.0f
-        if (noRatingGiven)
-        {
+        if (noRatingGiven) {
             showValidationErrorForRating("Rating is required")
         }
 
@@ -190,8 +174,7 @@ class DetailedListingActivity : AppCompatActivity()
     }
 
     // Function to show validation error for rating
-    private fun showValidationErrorForRating(message: String)
-    {
+    private fun showValidationErrorForRating(message: String) {
         ratingValidationText.text = message
         ratingValidationText.visibility = View.VISIBLE
         ratingValidationText.setTextColor(resources.getColor(R.color.errorText))
@@ -202,4 +185,74 @@ class DetailedListingActivity : AppCompatActivity()
         ratingValidationText.setTextColor(resources.getColor(R.color.defaultText))
         ratingValidationText.visibility = View.INVISIBLE
     }
+
+    /* FUNCTIONS FOR EDIT AND DELETE YOUR REVIEW SECTION */
+
+    // Function to show the edit dialog
+    fun showEditDialog(view: View) {
+        val dialogView = layoutInflater.inflate(R.layout.dialog_edit_review, null)
+        val dialogBuilder = AlertDialog.Builder(this)
+            .setView(dialogView)
+            .setTitle("Edit Review")
+
+        val editTitle = dialogView.findViewById<EditText>(R.id.editTitle)
+        val editContent = dialogView.findViewById<EditText>(R.id.editContent)
+        val saveChangesButton = dialogView.findViewById<ImageButton>(R.id.saveChangesButton)
+        val cancelEditButton = dialogView.findViewById<ImageButton>(R.id.cancelEditButton)
+
+        // Set existing review details to EditText fields if needed
+        // ...
+
+        val alertDialog = dialogBuilder.show()
+
+        // Set click listener for "Save Changes" button
+        saveChangesButton.setOnClickListener {
+            // Save changes logic goes here
+            showToast("Review Edited!")
+            // Refresh the UI or handle other actions after editing...
+            alertDialog.dismiss()
+        }
+
+        // Set click listener for "Cancel" button
+        cancelEditButton.setOnClickListener {
+            alertDialog.dismiss()
+        }
+    }
+
+    // Function to show the delete confirmation dialog
+    fun showDeleteConfirmationDialog(view: View) {
+        val dialogView = layoutInflater.inflate(R.layout.dialog_delete_confirmation, null)
+        val dialogBuilder = AlertDialog.Builder(this)
+            .setView(dialogView)
+            .setTitle("Delete Review")
+
+        val confirmDeleteButton = dialogView.findViewById<Button>(R.id.confirmDeleteButton)
+        val cancelDeleteButton = dialogView.findViewById<Button>(R.id.cancelDeleteButton)
+
+        val alertDialog = dialogBuilder.show()
+
+        // Set click listener for "Confirm" button
+        confirmDeleteButton.setOnClickListener {
+            // Delete the review logic goes here
+            showToast("Review Deleted!")
+            // Refresh the UI or handle other actions after deletion...
+            alertDialog.dismiss()
+        }
+
+        // Set click listener for "Cancel" button
+        cancelDeleteButton.setOnClickListener {
+            alertDialog.dismiss()
+        }
+    }
+
+    // Function to handle click events on editPostButton
+    fun onEditPostButtonClick(view: View) {
+        showEditDialog(view)
+    }
+
+    // Function to handle click events on deletePostButton
+    fun onDeletePostButtonClick(view: View) {
+        showDeleteConfirmationDialog(view)
+    }
+
 }
